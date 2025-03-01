ac75ecff857a12a952c33aeceaa02334f8ed1767
TheBolshoy <d-costa@mail.ru>
2025-03-01 22:07:28 +0300

    second commit

--- js/form.js
+++ js/form.js
@@ -1,28 +1,47 @@
-console.log('ah')
-import { format } from './node_modules/date-fns';
+//import { format } from '../node_modules/date-fns';
 
-const form = document.getElementById('input-form')
-const formInput = document.getElementById('message-textarea')
-const template = document.querySelector('.message')
+const form = document.querySelector('#input-form')
+const send = document.querySelector('.send-button')
+const formInput = document.querySelector('.message-textarea')
+const template = document.querySelector('#message')
 const templateContent = template.content.cloneNode(true);
+const messagesContainer = document.querySelector('.messenger')
+const messageTextMe = document.querySelector('.text-me')
+const messageText = formInput.value
 
-
-function renderMessageHTML(event) {
-    event.preventDefault()
-    const messageText = formInput.textContent
-    if (messageText.trim() !== '') {
-        createHTML()
-    }
-}
 
 function createHTML() {
     const messagePlate = document.createElement('div')
+    const messageText = formInput.value
+
     messagePlate.setAttribute('class', 'message-me plate')
-    const time = document.querySelector('.time')
-    time.textContent = format(new Date(), 'hh:MM')
+    //const time = document.querySelector('.time')
+    //time.textContent = format(new Date(), 'hh:MM')
+    const templateContent = template.content.cloneNode(true);
     messagePlate.append(templateContent);
-    messageText.value = ''
+    messagesContainer.appendChild(messagePlate)
+    //document.body.append(templateContent);
+    messagePlate.firstChild.textContent = messageText
+    //messagePlate.lastChild.textContent = format(new Date(), 'hh:MM')
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
 
-form.addEventListener('submit', renderMessageHTML())
+send.addEventListener('click', function (event) {
+    event.preventDefault()
+    const messageText = formInput.value
+    if (messageText.trim() !== '') {
+        createHTML()
+        formInput.value = ''
+    }
+})
+
+
+
+
+formInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        createHTML();
+        formInput.value = ''
+        event.preventDefault(); // Предотвращение перехода на новую строку
+    }
+});
